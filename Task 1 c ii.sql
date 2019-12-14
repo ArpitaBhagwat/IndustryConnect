de3a0cb523c23a34876884e5025456425ba8c7cf
ArpitaBhagwat <58862374+ArpitaBhagwat@users.noreply.github.com>
2019-12-14 13:02:00 +1300

    Add files via upload

---
+++ Task 1 c ii.sql
@@ -0,0 +1,5 @@
+SELECT dbo.PropertyFinance.PropertyId, dbo.PropertyFinance.Yield
+FROM dbo.PropertyFinance
+WHERE dbo.PropertyFinance.PropertyId = 5597
+OR dbo.PropertyFinance.PropertyId = 5637
+OR dbo.PropertyFinance.PropertyId = 5638
